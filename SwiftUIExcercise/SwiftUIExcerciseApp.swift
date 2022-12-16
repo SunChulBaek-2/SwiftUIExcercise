d0e33a6e36848750d7057762b7e20979487204df
x1210x <x1210x@gmail.com>
2022-12-16 14:09:15 +0900

    init

---
+++ SwiftUIExcercise/SwiftUIExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExcerciseApp.swift
+//  SwiftUIExcercise
+//
+//  Created by 백선철 on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
